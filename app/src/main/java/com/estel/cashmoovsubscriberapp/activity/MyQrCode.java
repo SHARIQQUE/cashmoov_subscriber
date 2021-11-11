8a1559c039380e99f84b7ab561090c2284b5f7ca
User <sharique9718@gmail.com>
2021-11-11 17:56:08 +0530

    commit qr abhay

--- app/src/main/java/com/estel/cashmoovsubscriberapp/activity/MyQrCode.java
+++ app/src/main/java/com/estel/cashmoovsubscriberapp/activity/MyQrCode.java
@@ -2,16 +2,26 @@ package com.estel.cashmoovsubscriberapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.estel.cashmoovsubscriberapp.MainActivity;
+import com.estel.cashmoovsubscriberapp.MyApplication;
 import com.estel.cashmoovsubscriberapp.R;
+import com.estel.cashmoovsubscriberapp.apiCalls.API;
+import com.estel.cashmoovsubscriberapp.apiCalls.Api_Responce_Handler;
+
+import org.json.JSONObject;
 
 public class MyQrCode extends AppCompatActivity {
     public static MyQrCode myqrcodeC;
-    ImageView imgBack,imgHome;
+    ImageView imgBack,imgHome,imgQR;
+    TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +61,76 @@ public class MyQrCode extends AppCompatActivity {
 
 
     private void getIds() {
-        // tvSend = findViewById(R.id.tvSend);
+        tvName = findViewById(R.id.tvName);
+        imgQR = findViewById(R.id.imgQR);
 
+        String fName = MyApplication.getSaveString("firstName", getApplicationContext());
+        String lName = MyApplication.getSaveString("lastName", getApplicationContext());
+      //  String mobile = MyApplication.getSaveString("mobile", getApplicationContext());
+        //String email = MyApplication.getSaveString("email", getApplicationContext());
 
+        String name = null;
+        if (fName != null && !fName.isEmpty() && !fName.equals("null")) {
+            name = fName;
+        }
+        if (lName != null && !lName.isEmpty() && !lName.equals("null")) {
+            name=fName+" "+lName;
+        }
+        tvName.setText(getString(R.string.hello)+", "+name);
+
+//        if (mobile != null && !mobile.isEmpty() && !mobile.equals("null")) {
+//            txt_mobile.setText(mobile);
+//        }
+
+        callApiGenerateQR();
 
     }
 
+    private void callApiGenerateQR() {
+        try{
 
+            JSONObject qrJson=new JSONObject();
+            qrJson.put("walletOwnerCode", MyApplication.getSaveString("walletOwnerCode", myqrcodeC));
+
+            System.out.println("QR request"+qrJson.toString());
+
+            MyApplication.showloader(myqrcodeC,"Please wait!");
+            API.POST_REQEST_WH_NEW("ewallet/api/v1/qrCode", qrJson, new Api_Responce_Handler() {
+                @Override
+                public void success(JSONObject jsonObject) {
+                    MyApplication.hideLoader();
+                    System.out.println("qrJson response======="+jsonObject.toString());
+
+                    if (jsonObject != null) {
+                        if(jsonObject.optString("resultCode", "N/A").equalsIgnoreCase("0")){
+                            displayQRCode(jsonObject.optString("qrCode"));
+                        }else if(jsonObject.optString("resultCode", "N/A").equalsIgnoreCase("2001")){
+                            MyApplication.showToast(myqrcodeC,getString(R.string.technical_failure));
+                        } else {
+                            MyApplication.showToast(myqrcodeC,jsonObject.optString("resultDescription", "N/A"));
+                        }
+                    }
+                }
+
+                @Override
+                public void failure(String aFalse) {
+
+                }
+            });
+
+        }catch (Exception e){
+
+        }
+
+    }
+
+    private void displayQRCode(String qrCode){
+        byte[] qrByteArray = Base64.decode(qrCode, Base64.DEFAULT);
+        Glide.with(this)
+                .asBitmap()
+                .load(qrByteArray)
+                .into(imgQR);
+
+    }
 
 }
