e2a313a5c402f269aefa3fd1321a9f8b70820866
User <sharique9718@gmail.com>
2022-03-07 19:04:10 +0530

    commit update latest changes 07 March by abhay

--- app/src/main/java/com/estel/cashmoovsubscriberapp/activity/login/LoginPin.java
+++ app/src/main/java/com/estel/cashmoovsubscriberapp/activity/login/LoginPin.java
@@ -527,7 +527,8 @@ public class LoginPin extends AppCompatActivity {
                         }else{
                             MyApplication.saveString("ImageName", "",loginpinC);
                         }
-                        callLogin();
+                        apiCallSecurity();
+                        //callLogin();
                     }catch (Exception e){
 
                     }
@@ -546,14 +547,66 @@ public class LoginPin extends AppCompatActivity {
         });
     }
 
-    public void callLogin(){
-        MyApplication.isFirstTime=true;
-        MyApplication.IsPromoCalled = true;
-        Intent i = new Intent(loginpinC, MainActivity.class);
-        startActivity(i);
-        finish();
-        Toast.makeText(loginpinC,getString(R.string.login_successful),Toast.LENGTH_LONG).show();
+    private void apiCallSecurity() {
+
+        API.GET("ewallet/api/v1/loginSecurity", new Api_Responce_Handler() {
+            @Override
+            public void success(JSONObject jsonObject) {
+
+                MyApplication.hideLoader();
+
+                try {
+
+
+                    // JSONObject jsonObject = new JSONObject("{\"transactionId\":\"1789408\",\"requestTime\":\"Wed Oct 20 16:05:19 IST 2021\",\"responseTime\":\"Wed Oct 20 16:05:19 IST 2021\",\"resultCode\":\"0\",\"resultDescription\":\"Transaction Successful\",\"walletOwnerUser\":{\"id\":2171,\"code\":\"101917\",\"firstName\":\"TATASnegal\",\"userName\":\"TATASnegal5597\",\"mobileNumber\":\"8888888882\",\"email\":\"kundan.kumar@esteltelecom.com\",\"walletOwnerUserTypeCode\":\"100000\",\"walletOwnerUserTypeName\":\"Supervisor\",\"walletOwnerCategoryCode\":\"100000\",\"walletOwnerCategoryName\":\"Institute\",\"userCode\":\"1000002606\",\"status\":\"Active\",\"state\":\"Approved\",\"gender\":\"M\",\"idProofTypeCode\":\"100004\",\"idProofTypeName\":\"MILITARY ID CARD\",\"idProofNumber\":\"44444444444\",\"creationDate\":\"2021-10-01T09:04:07.330+0530\",\"notificationName\":\"EMAIL\",\"notificationLanguage\":\"en\",\"createdBy\":\"100308\",\"modificationDate\":\"2021-10-20T14:59:00.791+0530\",\"modifiedBy\":\"100308\",\"addressList\":[{\"id\":3569,\"walletOwnerUserCode\":\"101917\",\"addTypeCode\":\"100001\",\"addTypeName\":\"Commercial\",\"regionCode\":\"100068\",\"regionName\":\"Boke\",\"countryCode\":\"100092\",\"countryName\":\"Guinea\",\"city\":\"100022\",\"cityName\":\"Dubreka\",\"addressLine1\":\"delhi\",\"status\":\"Inactive\",\"creationDate\":\"2021-10-01T09:04:07.498+0530\",\"createdBy\":\"100250\",\"modificationDate\":\"2021-10-03T09:52:57.407+0530\",\"modifiedBy\":\"100308\"}],\"workingDaysList\":[{\"id\":3597,\"walletOwnerUserCode\":\"101917\",\"weekdaysCode\":\"100000\",\"weekdaysName\":\"Monday\",\"openingTime\":\"10:00 AM\",\"closingTime\":\"6:00 PM\",\"creationDate\":\"2021-10-01T09:04:07.518+0530\"},{\"id\":3598,\"walletOwnerUserCode\":\"101917\",\"weekdaysCode\":\"100001\",\"weekdaysName\":\"Tuesday\",\"openingTime\":\"10:00 AM\",\"closingTime\":\"6:00 PM\",\"creationDate\":\"2021-10-01T09:04:07.528+0530\"},{\"id\":3599,\"walletOwnerUserCode\":\"101917\",\"weekdaysCode\":\"100002\",\"weekdaysName\":\"Wednesday\",\"openingTime\":\"10:00 AM\",\"closingTime\":\"6:00 PM\",\"creationDate\":\"2021-10-01T09:04:07.538+0530\"},{\"id\":3600,\"walletOwnerUserCode\":\"101917\",\"weekdaysCode\":\"100003\",\"weekdaysName\":\"Thursday\",\"openingTime\":\"10:00 AM\",\"closingTime\":\"6:00 PM\",\"creationDate\":\"2021-10-01T09:04:07.547+0530\"},{\"id\":3601,\"walletOwnerUserCode\":\"101917\",\"weekdaysCode\":\"100004\",\"weekdaysName\":\"Friday\",\"openingTime\":\"10:00 AM\",\"closingTime\":\"6:00 PM\",\"creationDate\":\"2021-10-01T09:04:07.556+0530\"},{\"id\":3602,\"walletOwnerUserCode\":\"101917\",\"weekdaysCode\":\"100005\",\"weekdaysName\":\"Saturday\",\"openingTime\":\"10:00 AM\",\"closingTime\":\"6:00 PM\",\"creationDate\":\"2021-10-01T09:04:07.565+0530\"},{\"id\":3603,\"walletOwnerUserCode\":\"101917\",\"weekdaysCode\":\"100006\",\"weekdaysName\":\"Sunday\",\"openingTime\":\"10:00 AM\",\"closingTime\":\"2:00 PM\",\"creationDate\":\"2021-10-01T09:04:07.573+0530\"}],\"macEnabled\":false,\"ipEnabled\":false,\"resetCredReqInit\":false,\"notificationTypeCode\":\"100000\"}}");
+
+
+                    String resultCode = jsonObject.getString("resultCode");
+                    String resultDescription = jsonObject.getString("resultDescription");
+
+                    if (resultCode.equalsIgnoreCase("0")) {
+                        MyApplication.isFirstTime=true;
+                        MyApplication.IsPromoCalled = true;
+                        Intent i = new Intent(loginpinC, MainActivity.class);
+                        startActivity(i);
+                        finish();
+                        Toast.makeText(loginpinC,getString(R.string.login_successful),Toast.LENGTH_LONG).show();
+
+
+                    } else {
+                        Toast.makeText(loginpinC, resultDescription, Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+
+
+                } catch (Exception e) {
+                    Toast.makeText(loginpinC, e.toString(), Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+
+
+            @Override
+            public void failure(String aFalse) {
+
+                MyApplication.hideLoader();
+                Toast.makeText(loginpinC, aFalse, Toast.LENGTH_SHORT).show();
+                finish();
+
+            }
+        });
+
+
     }
+//    public void callLogin(){
+//        MyApplication.isFirstTime=true;
+//        MyApplication.IsPromoCalled = true;
+//        Intent i = new Intent(loginpinC, MainActivity.class);
+//        startActivity(i);
+//        finish();
+//        Toast.makeText(loginpinC,getString(R.string.login_successful),Toast.LENGTH_LONG).show();
+//    }
     @Override
     public void onBackPressed() {
         new AlertDialog.Builder(this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT)
