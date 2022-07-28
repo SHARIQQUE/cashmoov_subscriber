08afbe9e4dddd7c31bfd188dda8938d8b6b7504b
User <sharique9718@gmail.com>
2022-07-28 09:36:58 +0530

    api added

--- app/src/main/java/com/estel/cashmoovsubscriberapp/MainActivity.java
+++ app/src/main/java/com/estel/cashmoovsubscriberapp/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //  ddd
+      //  dd
         mainC = this;
         MyApplication.hideKeyboard(mainC);
         MyApplication.setLang(mainC);
